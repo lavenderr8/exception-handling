7dc59e84f1c0faea6fc414098206c33cd81937da
lavander8 <lavander8@rambler.su>
2025-09-22 21:36:15 +0300

    Сообщения об ошибках исправлены на более понятные

--- program.py
+++ program.py
@@ -3,9 +3,9 @@ try:
     num_2 = float(input("Введите второе число: "))
     division_result = num_1 / num_2
 except ValueError:
-    print("Ошибка! Введите число")
+    print("Ошибка! Введите число вместо той ерунды которую Вы ввели")
 except ZeroDivisionError:
-    print("Ошибка: Деление на ноль!")
+    print("Ошибка: Деление на ноль недопустимо! Беспредел!")
 else:
     print(f"Результат: {division_result}")
 finally:
